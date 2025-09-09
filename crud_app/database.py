01f096cfb310c7251cebae0f575c589a917203a1
Kumbhakarn <178929455+Kumbhakarn@users.noreply.github.com>
2025-09-09 18:21:35 +0530

    Initial Commit - databse connection file

--- crud_app/database.py
+++ crud_app/database.py
@@ -2,19 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-                        # "database_name:///./test.db" (/.= current Directory)  (/test.db = file name)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" # giving database url 
+# Database URL (SQLite in this case)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-# Create_engine = makes a connection with database
+# Create engine
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thred':False}
-    )
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-# sessionmaker = makes new session to do our work 
-# each session repreasent a transaction 
-# autoflush = False : SQLAlchemany will not automatically flush the changes.
-# we have to commit for changes manually
+# SessionLocal: used for creating DB sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for all ORM models
 Base = declarative_base()
-# creates a base class for models to inherit from linking the python classes with the tables
